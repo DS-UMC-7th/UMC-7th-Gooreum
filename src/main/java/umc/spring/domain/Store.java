94c41a3504b24a845dd55c7c68197b99e71d0356
YYJ <yoon20220823@dukcung.ac.kr>
2024-11-03 23:49:49 +0900

    mission5

---
+++ src/main/java/umc/spring/domain/Store.java
@@ -0,0 +1,23 @@
+package umc.spring.domain;
+
+import javax.persistence.*;
+import lombok.*;
+import umc.spring.domain.base.BaseEntity;
+
+@Entity
+@Getter
+@Builder
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@AllArgsConstructor
+public class Store extends BaseEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String name;
+
+    private String address;
+
+    private Float score;
+}
---
+++ src/main/java/umc/spring/domain/mapping/MemberMission.java
@@ -0,0 +1,21 @@
+package umc.spring.domain.mapping;
+
+import javax.persistence.*;
+import lombok.*;
+import umc.spring.domain.base.BaseEntity;
+import umc.spring.domain.enums.MissionStatus;
+
+@Entity
+@Getter
+@Builder
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@AllArgsConstructor
+public class MemberMission extends BaseEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Enumerated(EnumType.STRING)
+    private MissionStatus status;
+}
